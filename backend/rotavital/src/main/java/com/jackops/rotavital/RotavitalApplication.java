99f81033275c8cfc04ee1e465c67c88d7d226403
luanvfm <luanventurafm2@gmail.com>
2026-09-20 14:07:35 -0300

    Added Spring Template and Initial Dependencies

---
+++ backend/rotavital/src/main/java/com/jackops/rotavital/RotavitalApplication.java
@@ -0,0 +1,13 @@
+package com.jackops.rotavital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotavitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotavitalApplication.class, args);
+	}
+
+}
---
+++ backend/rotavital/src/test/java/com/jackops/rotavital/RotavitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackops.rotavital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotavitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
